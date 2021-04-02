bb4e577871a949e7e3b09092c7308e5cd359e482
mista <mista@203.229.164.56>
2021-04-02 18:38:06 +0900

    10일차 개인 스터디 추가

---
+++ src/Study/n1427.java
@@ -0,0 +1,18 @@
+package Study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class n1427 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String num = br.readLine();
+		String[] arr = num.split("");
+		Arrays.sort(arr);
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i]);
+		}
+	}
+}
