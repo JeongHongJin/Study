5ccfc852c098fb4ce7d7c73f529283d9660d9e45
mista <mista@128.134.6.52>
2021-04-27 18:51:31 +0900

    27일차 팀 스터디 dp문제 기초부터 다시 공부 필요한듯...

---
+++ src/TeamStudy/n1026.java
@@ -0,0 +1,33 @@
+package TeamStudy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class n1026 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] A = new int[N];
+		int[] B = new int[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			B[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(A);
+		Arrays.sort(B);
+		int sum = 0;
+		int j = N - 1;
+		for (int i = 0; i < A.length; i++) {
+			sum += A[i] * B[j];
+			j--;
+		}
+		System.out.println(sum);
+	}
+}
---
+++ src/TeamStudy/n14501.java
@@ -0,0 +1,34 @@
+package TeamStudy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class n14501 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] T = new int[N + 2];
+		int[] P = new int[N + 2];
+		int[] dp = new int[N + 2];
+
+		StringTokenizer st;
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+		for (int i = N; i > 0; i--) {
+			int next = i + T[i];
+			if (next > N + 1) {
+				dp[i] = dp[i + 1];
+			} else {
+				dp[i] = Math.max(dp[i + 1], dp[next] + P[i]);
+			}
+		}
+		System.out.println(Arrays.toString(dp));
+		System.out.println(dp[1]);
+	}
+}
