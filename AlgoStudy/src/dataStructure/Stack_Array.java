bf881d272d8da201818435a0a0556c29f825ca29
unknown <mistake9518@gmail.com>
2022-07-05 12:59:24 +0900

    feat: stack_array

---
+++ AlgoStudy/src/dataStructure/Stack_Array.java
@@ -0,0 +1,38 @@
+package dataStructure;
+
+public class Stack_Array {
+	int top;
+	int size;
+	int[] stack;
+
+	public Stack_Array(int size) {
+		this.size = size;
+		stack = new int[size];
+		top = -1;
+	}
+
+	public void push(int item) {
+		stack[++top] = item;
+		System.out.println(stack[top] + " Push");
+	}
+
+	public void pop() {
+		if (size == 0) {
+			return;
+		}
+		int pop = stack[top];
+		System.out.println(pop + " Pop");
+		stack[top--] = 0;
+	}
+
+	public void peek() {
+		if (size == 0) {
+			return;
+		}
+		System.out.println(stack[top]);
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+}
